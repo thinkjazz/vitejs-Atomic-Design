72dd5918fb5226d115317c48d98c547b70cf4b72
thinkjazz <lookreciuspin@gmail.com>
2022-05-10 03:12:34 +0300

    2 files modified, 1 added

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-debug-screens'),
+  ],
+};
