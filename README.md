6ab38db3743cdf76a2046274771ad7c4e881fdf4
thinkjazz <lookreciuspin@gmail.com>
2022-05-10 03:20:01 +0300

    3 files modified, 1 added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# vitejs-vite-tdpwk9
+# Atomic Design
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-tdpwk9)
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-atomic-design)
---
+++ src/components/TemplateLanding.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="flex flex-col min-h-screen">
+    <OrganismHeader :links="links" />
+    <div class="flex-auto flex flex-wrap bg-white">
+      <OrganismBanner class="w-full md:w-3/4 text-center md:text-left" />
+      <div class="container mx-auto px-4 py-8 md:py-16">
+        <OrganismGrid :cards="cards" />
+      </div>
+    </div>
+    <OrganismFooter
+      :columnOneLinks="columnOneLinks"
+      :columnTwoLinks="columnTwoLinks"
+    />
+  </section>
+</template>
+
+<script>
+import OrganismHeader from './OrganismHeader.vue';
+import OrganismFooter from './OrganismFooter.vue';
+import OrganismBanner from './OrganismBanner.vue';
+import OrganismGrid from './OrganismGrid.vue';
+
+export default {
+  name: 'TemplateLanding',
+  components: {
+    OrganismHeader,
+    OrganismFooter,
+    OrganismBanner,
+    OrganismGrid,
+  },
+  data: () => {
+    return {
+      links: [{ name: 'Header link 1', url: '#' }],
+      cards: [
+        {
+          title: 'Card 1',
+          text: 'Sed gravida libero sem, sed ornare arcu venenatis at.',
+        },
+        {
+          title: 'Card 2',
+          text: 'Etiam in arcu lectus. Nulla facilisi. Nunc viverra vehicula nunc eu tristique.',
+        },
+        {
+          title: 'Card 3',
+          text: 'Sed gravida libero sem, etiam in arcu lectus.',
+        },
+      ],
+      columnOneLinks: [
+        { name: 'Footer column 1', url: '#' },
+        { name: 'Footer column 1', url: '#' },
+      ],
+      columnTwoLinks: [
+        { name: 'Footer column 2', url: '#' },
+        { name: 'Footer column 2', url: '#' },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
